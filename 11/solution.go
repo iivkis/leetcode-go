358f9b85f5c7a0a2098149fa16f072139e41bea4
iivkis <razmolodinivan@mail.ru>
2025-06-04 15:16:14 +0700

    Implement maxArea function to calculate maximum water container area

---
+++ 11/solution.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// https://www.youtube.com/watch?v=UuiTKBwPgAo&ab_channel=NeetCode
+func maxArea(height []int) int {
+	var up, down int
+	var maxS int
+
+	down = len(height) - 1
+
+	for up < down {
+		if S := min(height[up], height[down]) * (down - up); S > maxS {
+			maxS = S
+		}
+
+		if height[up] > height[down] {
+			down -= 1
+		} else {
+			up += 1
+		}
+	}
+
+	return maxS
+}
+
+func main() {
+	arr := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	fmt.Println(maxArea(arr))
+}
